547e9ae3c0f5ea4bdba08e6e2a8dac9981c9ae8a
codemastershrey <79367994+codemastershrey@users.noreply.github.com>
2022-10-15 09:31:50 +0530

    Create Inverted_Double_Triangles.java

---
+++ Java/Inverted_Double_Triangles.java
@@ -0,0 +1,88 @@
+//Inverted_Double_Triangles
+import java.util.Scanner;
+
+public class Inverted_Double_Triangles
+{
+
+    public static void main(String[] args) 
+
+    {
+
+        Scanner sc = new Scanner(System.in);        
+
+        //Taking rows value from the user        
+
+        System.out.println("How many rows you want in this pattern?");       
+
+        int rows = sc.nextInt();      
+
+        System.out.println("Here is your pattern");        
+
+        //Printing upper half of the pattern    
+        for (int i = 1; i <= rows; i++) 
+
+        {
+
+            //Printing i spaces at the beginning of each row             
+
+            for (int j = 1; j < i; j++) 
+
+            {
+
+                System.out.print(" ");
+
+            }             
+
+            //Printing i to rows value at the end of each row             
+
+            for (int j = i; j <= rows; j++) 
+
+            { 
+
+                System.out.print(j); 
+
+            }              
+
+            System.out.println(); 
+
+        }          
+
+        //Printing lower half of the pattern          
+
+        for (int i = rows-1; i >= 1; i--) 
+
+        {
+
+            //Printing i spaces at the beginning of each row             
+
+            for (int j = 1; j < i; j++) 
+
+            {
+
+                System.out.print("");
+
+            }            
+
+            //Printing i to rows value at the end of each row            
+
+            for (int j = i; j <= rows; j++)
+
+            {
+
+                System.out.print(j);
+
+            }             
+
+            System.out.println();
+
+        }        
+
+        //Closing the resources         
+
+        sc.close();
+
+    }
+
+        
+
+}
