96517335426931ae9322884c73ecec34517f9717
Rohit Shailesh <90341417+imx0x@users.noreply.github.com>
2022-10-20 01:25:10 +0530

    leetcode  4sum problem
    leetcode 4sum problem with 16ms

--- CPP/arrays/4sum.cpp
+++ CPP/arrays/4sum.cpp
@@ -1,10 +1,10 @@
-# leetcode -- 18. 4Sum
-#Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+// # leetcode -- 18. 4Sum
+// #Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
 
-# 0 <= a, b, c, d < n
-# a, b, c, and d are distinct.
-# nums[a] + nums[b] + nums[c] + nums[d] == target
-# You may return the answer in any order.
+// # 0 <= a, b, c, d < n
+// # a, b, c, and d are distinct.
+// # nums[a] + nums[b] + nums[c] + nums[d] == target
+// # You may return the answer in any order.
 
 class Solution {
 public:
