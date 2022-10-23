ed12cf9fab017a67676378bc4ff20e674942b508
Tanu Singh <nagvanshitanu@gmail.com>
2022-10-24 00:59:58 +0530

    Create arraylength.py

---
+++ arraylength.py
@@ -0,0 +1,9 @@
+Fruit_List = ['apple', 'banana', 'orange', 'grapes']
+
+list_count = 0; 
+for fruit in Fruit_List:
+  list_count += 1
+
+ 
+print ("List length count  = ", list_count)
+#OUTPUT List length count = 4 
